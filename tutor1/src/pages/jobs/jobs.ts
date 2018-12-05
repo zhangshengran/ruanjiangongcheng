d43c96fe154c0f8236ca3a811829b830c4d5111d
zhangshengran <1286693585@qq.com>
2018-12-05 09:08:21 +0800

    前端页面编辑

---
+++ tutor1/src/pages/jobs/jobs.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the JobsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-jobs',
+  templateUrl: 'jobs.html',
+})
+export class JobsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  goback5(){
+    this.navCtrl.pop();
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad JobsPage');
+  }
+
+}
