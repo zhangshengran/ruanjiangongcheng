8cbf999a2e3c0cc12d9e70a34003de6d545084c7
BDX1997 <1165140257@qq.com>
2018-12-07 09:26:29 +0800

    123

--- people/Bo_Dongxiao/nodejs/01-hello-world/01-hello-world.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,res){
-    res.writeHeader(200,{'Content-Type':'text/plain'});
-    res.end('Hello World!');
-}).listen(3000);
-
-console.log("Server is starting...");
